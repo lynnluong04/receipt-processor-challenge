f1b325e93012d25405306371b74649d4d11f2b76
Lynn Luong <lynn.h.luong@gmail.com>
2023-10-17 23:11:22 -0400

    structs defined

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ import (
 
 )
 
+type Item struct {
+	shortDescription string
+	price string
+}
+
+type Receipt struct {
+	retailer string
+	purchaseDate string
+	purchaseTime string
+	items []Item
+	total string
+}
+
 func main() {
 	fmt.Println("Hello")
 }
